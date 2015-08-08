68693cd9d68d20e1e8780407ee4aa826b4c759b2
Guillaume Vrilliaux <vrilliaux.guillaume@hotmail.com>
2015-08-09 01:25:20 +0200

    Mise en forme (version V1) du contenu fictif sur la page "club et sites", corrections de la hauteur de police en responsive du header

--- vue/corps_clubetsites.php
+++ vue/corps_clubetsites.php
@@ -1,70 +1,82 @@
 <section id="clubEtSites" class="row">
-	<div class="col-xs-12">
-		<h1> Voler a Grenois </h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum metus quis libero porttitor mollis. 
-                    Praesent felis risus, condimentum at dapibus porta, volutpat vitae mi. Nulla eu tristique nisl, eget sodales orci. 
-                    Vivamus quis tempus velit, at molestie mauris. Mauris non massa placerat, ullamcorper velit non, accumsan sapien. 
-                    Mauris accumsan tristique tellus, sit amet fermentum diam. Donec accumsan scelerisque sagittis. 
-                    Vestibulum consequat pharetra enim. Mauris in libero suscipit, facilisis mauris vel, sagittis ipsum. 
-                    Fusce tellus libero, cursus bibendum eros pellentesque, posuere porta velit. Sed pellentesque gravida enim ut bibendum.
-                </p>
-                <div class="row pastille-space">
-                    <div class="col-xs-6 pastille-container">
-                        <h1>Grenois</h1><br />
-                        <img class="pastille" src="static/images/incontournables/pastilleGrenois" />
-                    </div>
-                    <div class="col-xs-6 pastille-container">
-                        <h1>Asnan</h1><br />
-                        <img class="pastille" src="static/images/incontournables/pastilleAsnan" />
-                    </div>
-                </div>
-                <p>
-                    Sed odio ipsum, lobortis id lobortis dignissim, tempus at risus. Mauris sem leo, aliquet vel congue a, fermentum vel diam. 
-                    Vivamus at mattis turpis. Nulla tortor arcu, iaculis vel facilisis id, blandit ac metus. 
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam gravida ipsum ut nibh hendrerit eleifend. 
-                    Phasellus cursus eget turpis a imperdiet. Vestibulum a enim sapien. Maecenas est augue, suscipit et erat nec, aliquet accumsan libero. 
-                    Vestibulum lacinia sit amet nibh eu placerat. Vestibulum et diam mollis, sodales felis eu, lobortis lorem. 
-                    Nullam magna enim, posuere non bibendum quis, iaculis id nunc. Quisque pellentesque semper viverra. 
-                    Vivamus sit amet ligula ullamcorper, placerat felis in, luctus quam.
-                </p>
-                <p>
-                    Phasellus a elit ultrices, suscipit tortor quis, consectetur ipsum. Integer sed est libero. Nunc sollicitudin molestie rutrum. 
-                    Vestibulum non diam odio. Donec eu eros a eros accumsan posuere. Donec convallis sem ultrices, vestibulum magna ut, volutpat nibh. 
-                    Aenean iaculis facilisis ante non mollis. Suspendisse potenti.
-                </p>
-                <p>
-                    Duis ut diam id lacus accumsan imperdiet. Cras luctus purus a consequat ultricies. 
-                    Suspendisse vestibulum, magna quis pretium molestie, justo ante ullamcorper erat, eget rutrum diam diam vel dolor. 
-                    Ut maximus ornare orci, a iaculis metus varius in. Proin quis volutpat massa. Etiam at mollis magna. Donec vel nibh id mauris efficitur imperdiet.
-                </p>
-                <p>
-                    Phasellus luctus bibendum lectus in scelerisque. Mauris sagittis arcu sed mi scelerisque lobortis. Donec quis aliquet elit. Curabitur at iaculis neque. 
-                    Mauris vitae lorem eu felis sagittis ultrices non eu ex. Donec a dignissim ligula. Etiam sem felis, bibendum quis velit eget, mattis rutrum lectus. 
-                    Nam arcu purus, consectetur vitae mi nec, sollicitudin finibus tortor. Donec elit nisi, sollicitudin id efficitur nec, congue ac tortor. 
-                    Phasellus ornare neque enim, nec imperdiet diam hendrerit vel. Duis gravida dolor vitae porta luctus. Sed sed gravida sapien, non ultrices nibh. 
-                    Pellentesque sed maximus tellus. Phasellus sapien enim, consequat vitae mauris sed, eleifend dapibus nisl.
-                </p>
-		<ul>
-			<li>Historique du club? (Dates cl&eacute;s, photos d'&eacute;poque, etc.)</li>
-			<li>Ce n'est ni un poulet, ni une autruche, ni un dindon: c'est un Dodo! Non nous ne sommes pas r&eacute;unionnais, alors pourquoi un Dodo? -> Le mythe...</li>
-			<li>Nos activit&eacutes;s:</li>
-			<ul>
-				<li>Pas de biplace (clubs de la r&eacute;gion qui proposent?)</li>
-				<li>Pas de formation car pas une &eacute;cole (clubs de la r&eacute;gion qui proposent?)</li>
-			</ul>
-			<li>Les sites de vol?</li>
-			<ul>
-				<li>deco / atterro de Grenois</li>
-				<li>deco / atterro dans les chemins ou les champs coup&eacute;s d'Asnan (pas d'atterro)</li>
-			</ul>
-			<li>Les orientations des sites (Grenois SE-S-SO, Asnan O-SO)</li>
-			<li>Les dangers?</li>
-			<li>Travers&eacute;e du village au pas (30km/h)</li>
-			<li>Grenois: ne pas se garer sur le deco ou l'atterro</li>
-			<li>Asnan: ne pas se garer dans le champs sous le deco</li>
-			<br />
-			<li>On peut faire le lien avec <?php echo'<a href="">les habitants de Grenois et la commune</a>'; ?></li>
-		</ul>
-	</div>
+    <div class="col-xs-12">
+        <h1> Voler a Grenois </h1>
+        <br />
+        <h3>Les Sites de vol</h3>
+        <div class="historySection">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum metus quis libero porttitor mollis. 
+                Praesent felis risus, condimentum at dapibus porta, volutpat vitae mi. Nulla eu tristique nisl, eget sodales orci. 
+                Vivamus quis tempus velit, at molestie mauris. Mauris non massa placerat, ullamcorper velit non, accumsan sapien. 
+                Mauris accumsan tristique tellus, sit amet fermentum diam. Donec accumsan scelerisque sagittis. 
+                Vestibulum consequat pharetra enim. Mauris in libero suscipit, facilisis mauris vel, sagittis ipsum. 
+                Fusce tellus libero, cursus bibendum eros pellentesque, posuere porta velit. Sed pellentesque gravida enim ut bibendum.
+            </p>
+        </div>
+        <div class="row pastille-space">
+            <div class="col-xs-6 pastille-container">
+                <h3>Grenois</h3><br />
+                <img class="pastille" src="static/images/incontournables/pastilleGrenois.jpg" />
+            </div>
+            <div class="col-xs-6 pastille-container">
+                <h3>Asnan</h3><br />
+                <img class="pastille" src="static/images/incontournables/pastilleAsnan.jpg" />
+            </div>
+        </div>
+        <br /><br />
+        <h3>Historique</h3>
+        <div class="historySection">
+            <p>
+                Sed odio ipsum, lobortis id lobortis dignissim, tempus at risus. Mauris sem leo, aliquet vel congue a, fermentum vel diam. 
+                Vivamus at mattis turpis. Nulla tortor arcu, iaculis vel facilisis id, blandit ac metus. 
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam gravida ipsum ut nibh hendrerit eleifend. 
+                Phasellus cursus eget turpis a imperdiet. Vestibulum a enim sapien. Maecenas est augue, suscipit et erat nec, aliquet accumsan libero. 
+                Vestibulum lacinia sit amet nibh eu placerat. Vestibulum et diam mollis, sodales felis eu, lobortis lorem. 
+                Nullam magna enim, posuere non bibendum quis, iaculis id nunc. Quisque pellentesque semper viverra. 
+                Vivamus sit amet ligula ullamcorper, placerat felis in, luctus quam.
+            </p>
+        </div>
+        <div class="historyPic"><img src="static/images/incontournables/img213.jpg" /></div>
+        <div class="historySection">
+            <p>
+                Duis ut diam id lacus accumsan imperdiet. Cras luctus purus a consequat ultricies. 
+                Suspendisse vestibulum, magna quis pretium molestie, justo ante ullamcorper erat, eget rutrum diam diam vel dolor. 
+                Ut maximus ornare orci, a iaculis metus varius in. Proin quis volutpat massa. Etiam at mollis magna. Donec vel nibh id mauris efficitur imperdiet.
+            </p>
+        </div>
+        <div class="historySection separated">
+            <table>
+                <tr class="texte">
+                    <td>
+                        Phasellus a elit ultrices, suscipit tortor quis, consectetur ipsum. Integer sed est libero. Nunc sollicitudin molestie rutrum. 
+                        Vestibulum non diam odio. <br />
+                        Donec eu eros a eros accumsan posuere. Donec convallis sem ultrices, vestibulum magna ut, volutpat nibh. 
+                        Aenean iaculis facilisis ante non mollis. Suspendisse potenti.
+                    </td>
+                </tr>
+                <tr class="picture">
+                    <td>
+                        <img src="static/images/incontournables/img212.jpg" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <br /><br />
+        <h3>Le club et ses membres</h3>
+        <div class="historySection">
+            <p>
+                Phasellus luctus bibendum lectus in scelerisque. Mauris sagittis arcu sed mi scelerisque lobortis. Donec quis aliquet elit. Curabitur at iaculis neque. 
+                Mauris vitae lorem eu felis sagittis ultrices non eu ex. Donec a dignissim ligula. Etiam sem felis, bibendum quis velit eget, mattis rutrum lectus. 
+                Nam arcu purus, consectetur vitae mi nec, sollicitudin finibus tortor. Donec elit nisi, sollicitudin id efficitur nec, congue ac tortor. 
+                Phasellus ornare neque enim, nec imperdiet diam hendrerit vel.
+            </p>
+            <br /><br />
+            <div class="historyPic"><img src="static/images/incontournables/100_9707.jpg" /></div>
+            <br /><br />
+            <p>
+                Duis gravida dolor vitae porta luctus. Sed sed gravida sapien, non ultrices nibh. 
+                Pellentesque sed maximus tellus. Phasellus sapien enim, consequat vitae mauris sed, eleifend dapibus nisl.
+            </p>
+        </div>
+    </div>
 </section>
--- vue/corps_contact.php
+++ vue/corps_contact.php
@@ -2,13 +2,7 @@
 	<div class="col-xs-12">
 		<h1> Contact </h1>
 		<p>
-			<ul>
-				<li>Une question a adresser au club? => <a href="">Dominique Chagny (pr&eacute;sident)</a> 
-					// On peut aussi utiliser la boite mail du club associ&eacute;e au site: <a href="">vollibre58@free.fr</a></li>
-				<li>Une remarque sur le site internet? => <a href="">webmaster.vl58@free.fr</a> // adresse secondaire free</li>
-				<li>Vous etes venu voler et vous souhaitez partager ou recuperer du contenu (photos, videos, etc.)? => <a href="">redac.vl58@free.fr</a> // adresse secondaire free</li>
-				<li>etc.</li>
-			</ul>
+			
 		</p>
 	</div>
 </section>

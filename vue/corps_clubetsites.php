d03400be42a98f38c7e05312b9332602012344a5
guillaume <g.vrilliaux@gmail.com>
2015-08-08 18:33:45 +0200

    ajout de contenu fictif dans la partie club et sites

--- vue/corps_clubetsites.php
+++ vue/corps_clubetsites.php
@@ -1,6 +1,50 @@
 <section id="clubEtSites" class="row">
 	<div class="col-xs-12">
 		<h1> Voler a Grenois </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum metus quis libero porttitor mollis. 
+                    Praesent felis risus, condimentum at dapibus porta, volutpat vitae mi. Nulla eu tristique nisl, eget sodales orci. 
+                    Vivamus quis tempus velit, at molestie mauris. Mauris non massa placerat, ullamcorper velit non, accumsan sapien. 
+                    Mauris accumsan tristique tellus, sit amet fermentum diam. Donec accumsan scelerisque sagittis. 
+                    Vestibulum consequat pharetra enim. Mauris in libero suscipit, facilisis mauris vel, sagittis ipsum. 
+                    Fusce tellus libero, cursus bibendum eros pellentesque, posuere porta velit. Sed pellentesque gravida enim ut bibendum.
+                </p>
+                <div class="row pastille-space">
+                    <div class="col-xs-6 pastille-container">
+                        <h1>Grenois</h1><br />
+                        <img class="pastille" src="static/images/incontournables/pastilleGrenois" />
+                    </div>
+                    <div class="col-xs-6 pastille-container">
+                        <h1>Asnan</h1><br />
+                        <img class="pastille" src="static/images/incontournables/pastilleAsnan" />
+                    </div>
+                </div>
+                <p>
+                    Sed odio ipsum, lobortis id lobortis dignissim, tempus at risus. Mauris sem leo, aliquet vel congue a, fermentum vel diam. 
+                    Vivamus at mattis turpis. Nulla tortor arcu, iaculis vel facilisis id, blandit ac metus. 
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam gravida ipsum ut nibh hendrerit eleifend. 
+                    Phasellus cursus eget turpis a imperdiet. Vestibulum a enim sapien. Maecenas est augue, suscipit et erat nec, aliquet accumsan libero. 
+                    Vestibulum lacinia sit amet nibh eu placerat. Vestibulum et diam mollis, sodales felis eu, lobortis lorem. 
+                    Nullam magna enim, posuere non bibendum quis, iaculis id nunc. Quisque pellentesque semper viverra. 
+                    Vivamus sit amet ligula ullamcorper, placerat felis in, luctus quam.
+                </p>
+                <p>
+                    Phasellus a elit ultrices, suscipit tortor quis, consectetur ipsum. Integer sed est libero. Nunc sollicitudin molestie rutrum. 
+                    Vestibulum non diam odio. Donec eu eros a eros accumsan posuere. Donec convallis sem ultrices, vestibulum magna ut, volutpat nibh. 
+                    Aenean iaculis facilisis ante non mollis. Suspendisse potenti.
+                </p>
+                <p>
+                    Duis ut diam id lacus accumsan imperdiet. Cras luctus purus a consequat ultricies. 
+                    Suspendisse vestibulum, magna quis pretium molestie, justo ante ullamcorper erat, eget rutrum diam diam vel dolor. 
+                    Ut maximus ornare orci, a iaculis metus varius in. Proin quis volutpat massa. Etiam at mollis magna. Donec vel nibh id mauris efficitur imperdiet.
+                </p>
+                <p>
+                    Phasellus luctus bibendum lectus in scelerisque. Mauris sagittis arcu sed mi scelerisque lobortis. Donec quis aliquet elit. Curabitur at iaculis neque. 
+                    Mauris vitae lorem eu felis sagittis ultrices non eu ex. Donec a dignissim ligula. Etiam sem felis, bibendum quis velit eget, mattis rutrum lectus. 
+                    Nam arcu purus, consectetur vitae mi nec, sollicitudin finibus tortor. Donec elit nisi, sollicitudin id efficitur nec, congue ac tortor. 
+                    Phasellus ornare neque enim, nec imperdiet diam hendrerit vel. Duis gravida dolor vitae porta luctus. Sed sed gravida sapien, non ultrices nibh. 
+                    Pellentesque sed maximus tellus. Phasellus sapien enim, consequat vitae mauris sed, eleifend dapibus nisl.
+                </p>
 		<ul>
 			<li>Historique du club? (Dates cl&eacute;s, photos d'&eacute;poque, etc.)</li>
 			<li>Ce n'est ni un poulet, ni une autruche, ni un dindon: c'est un Dodo! Non nous ne sommes pas r&eacute;unionnais, alors pourquoi un Dodo? -> Le mythe...</li>

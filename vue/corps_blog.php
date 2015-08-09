cd77c695f52d2b5087a3fa9644901afad3fecb77
Guillaume Vrilliaux <vrilliaux.guillaume@hotmail.com>
2015-08-09 13:02:48 +0200

    Mise en forme (version V1) du contenu fictif sur la page "Blog"

--- vue/corps_blog.php
+++ vue/corps_blog.php
@@ -1,8 +1,62 @@
 <section id="blog" class="row">
-	<div class="col-xs-12">
-		<h1> Le blog </h1>
-		<p>
-			
-		</p>
-	</div>
+    <div class="col-xs-12">
+        <h1> Le blog </h1>
+        <div class="albumsLinks">
+            <h4>Albums des photographes:</h4>
+            <a href="">Album de Guillaume</a>
+            <a href="">Album de Sebastien</a>
+        </div>
+        <div class="blogArticle">
+            <h3>Nos vols d'automne 2014</h3>
+            <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed nunc odio, semper sit amet enim a, scelerisque facilisis erat. 
+                Phasellus sit amet scelerisque felis. Aliquam interdum nisi eu lectus facilisis aliquet. 
+                In dapibus tristique nisi, vitae consequat libero viverra non. Suspendisse ac velit posuere, 
+                lobortis quam vitae, suscipit justo. Aliquam molestie ornare eros, sed blandit arcu tincidunt at. 
+                Phasellus dictum interdum pharetra. Nunc in leo pharetra, interdum nisi eu, vestibulum justo. 
+                Cras at dolor erat. Integer vulputate nisi vitae nibh eleifend faucibus. Mauris imperdiet eleifend 
+                ex a auctor.
+            </div>
+            <div class="picture">
+                <img src="static/images/fonds/fond07.jpg" />
+            </div>
+            <div class="content">
+                Aliquam vestibulum lectus sit amet felis eleifend, vitae lobortis erat commodo. Donec rhoncus 
+                libero nec enim lacinia scelerisque non et dolor. Class aptent taciti sociosqu ad litora torquent 
+                per conubia nostra, per inceptos himenaeos. Suspendisse ac enim non lacus blandit finibus. 
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                Cras dictum erat sit amet metus finibus, id aliquet est tempus. Aliquam sed eros sapien. Donec 
+                facilisis faucibus sapien id suscipit. Pellentesque efficitur diam sit amet vehicula euismod. 
+                Nam sed euismod ligula. Praesent magna lacus, vestibulum sit amet cursus at, consectetur non justo. 
+                Quisque et laoreet nunc.
+            </div>
+        </div>
+        <div class="blogArticle">
+            <h3>Nos vols d'&eacute;t&eacute; 2014</h3>
+            <div class="galerie-3pic">
+                <table>
+                    <tr>
+                        <td class="col-large col-left">
+                            <img class="pic-large" src="static/images/fonds/fond02.jpg" />
+                        </td>
+                        <td class="col-small col-right">
+                            <img class="pic-medium" src="static/images/fonds/fond05.jpg" />
+                            <img  class="pic-medium" src="static/images/fonds/fond08.jpg" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed nunc odio, semper sit amet enim a, scelerisque facilisis erat. 
+                Phasellus sit amet scelerisque felis. Aliquam interdum nisi eu lectus facilisis aliquet. 
+                In dapibus tristique nisi, vitae consequat libero viverra non. Suspendisse ac velit posuere, 
+                lobortis quam vitae, suscipit justo. Aliquam molestie ornare eros, sed blandit arcu tincidunt at. 
+                Phasellus dictum interdum pharetra. Nunc in leo pharetra, interdum nisi eu, vestibulum justo. 
+                Cras at dolor erat. Integer vulputate nisi vitae nibh eleifend faucibus. Mauris imperdiet eleifend 
+                ex a auctor.
+            </div>
+        </div>
+    </div>
 </section>

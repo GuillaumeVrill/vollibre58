33d731934da1209ade9a12d254f5017e78c01bec
Guillaume Vrilliaux <vrilliaux.guillaume@hotmail.com>
2015-09-19 16:36:47 +0200

    preparation des icones de suppression des evenements et des alertes, annotation des fonctions nécessaires dans le README.md avec exemples de pour les fonctions spécifiques (à mettre à jour lorsqu'une fonction est prête)

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # vollibre58
 Dépôt de sauvegarde du site vollibre58
 Ne pas Commit le fichier "nbproject" généré par netbeans.
+
+
+-----------------------------------------------------------------
+Fonctions relatives à la base de données qu'il faut créer:
+-----------------------------------------------------------------
+
+--- FONCTION NAME ---                       Done? (OK)
+#ExempleFonction()                          -OK
+
+SupprimerEvent($event_id);                  -
+SupprimerAlerte($alerte_id);                -
+SupprimerArticle($article_id);              -
+SupprimerUtilisateur($user_id);             -
+
+recupererTousLesArticles();                 -
+recupererArticle($article_id);              -
+
+recupererTouteslesImages();                 -
+recupererImagesArticle($article_id);        -
+
+
+
+NOTES: Pour la recupération d'un article ou de tous les articles, on récupèrera également les images de chaque article, il peut en posséder 1, 2, ou 3.
+Le tableau de retour pour l'ensemble des articles pourra ressembler à ça:
+//--- $article[indice][colonneBDD][indice-secondaire-eventuel] = valeur; ---//
+$article[0]['id'] = "1";
+$article[0]['txt1'] = "blablabla";
+$article[0]['txt2'] = "blabla";
+$article[0]['disposition'] = "4"; //correspond à la disposition avec 2 textes et 2 images.
+$article[0]['img'][0] = "http://site-hebergement.fr/mon-image.png";
+$article[0]['img'][1] = "http://site-hebergement.fr/mon-image2.JPG";
+$article[1]['id'] = "2";
+$article[1]['txt1'] = "blablablabla";
+$article[1]['txt2'] = "blablablablablabla";
+$article[1]['disposition'] = "1"; //correspond à la disposition standart avec 2 textes et une image
+$article[1]['img'][0] = "http://site-hebergement.fr/mon-image3.jpg";
+//etc.
--- vue/corps_evenements.php
+++ vue/corps_evenements.php
@@ -1,46 +1,80 @@
+<?php
+    //récupération des événements et des alertes:
+    $evenements = recupererEvenements();
+    $alertes = recupererAlertes();
+    
+    //traitement des formulaires de suppression:
+    for($i=0; $i<sizeof($evenements); $i++){
+        if(isset($_POST['event'.$i])){
+            
+        }
+    }
+    for($i=0; $i<sizeof($alertes); $i++){
+        if(isset($_POST['alerte'.$i])){
+            
+        }
+    }
+?>
+
 <section id="events" class="row">
-	<div class="col-xs-12 col-sm-6" id="event">
-		<div class="panel panel-success">
-			<div class="panel-heading">
-				<h1 class="panel-title">Ev&eacute;nements</h1>
-			</div>
-			<ul class="list-group">
-				<?php
-				$evenements = recupererEvenements();
-				
-				if(isset($evenements)){
-					for($i=0; $i<count($evenements); $i++){ ?>
-                                            <li class="list-group-item">
-                                            <span class="badge">Evenement</span>
-                                            <?php print utf8_encode($evenements[$i]->getDateFin()); ?> - 
-                                            <strong><?php print utf8_encode($evenements[$i]->getTitre()); ?></strong> - 
-                                            <?php print utf8_encode ($evenements[$i]->getDescription()); ?>
-                                            </li>
-					<?php }
-				} ?>
-			</ul>
-		</div>
-	</div>
-	<div class="col-xs-12 col-sm-6" id="alerte">
-		<div class="panel panel-danger">
-			<div class="panel-heading">
-				<h1 class="panel-title">Alertes</h1>
-			</div>
-			<ul class="list-group">
-				<?php
-                                $alertes = recupererAlertes();
-                                
-                                if(isset($alertes)){
-                                    for($i=0; $i<count($alertes); $i++){ ?>
-                                            <li class="list-group-item">
-                                            <span class="badge">Alerte</span>
-                                            <?php print utf8_encode($alertes[$i]->getDateDebut()); ?> - 
-                                            <strong><?php print utf8_encode($alertes[$i]->getTitre()); ?></strong> - 
-                                            <?php print utf8_encode ($alertes[$i]->getDescription()); ?>
-                                            </li>
-                                    <?php } 
-                                } ?>
-			</ul>
-		</div>
-	</div>
+    <div class="col-xs-12 col-sm-6" id="event">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h1 class="panel-title">Ev&eacute;nements</h1>
+            </div>
+            <ul class="list-group">
+                <?php if(isset($evenements)){
+                    for($i=0; $i<count($evenements); $i++){ ?>
+                        <li class="list-group-item">
+                            <div class="contenu">
+                                <span class="badge">Evenement</span>
+                                <?php print utf8_encode($evenements[$i]->getDateFin()); ?> - 
+                                <strong><?php print utf8_encode($evenements[$i]->getTitre()); ?></strong> - 
+                                <?php print utf8_encode ($evenements[$i]->getDescription()); ?>
+                            </div>
+                            <?php if(isset($_SESSION['user_right']) && !empty($_SESSION['user_right']) && 
+                                    ($_SESSION['user_right']=='moderateur' || $_SESSION['user_right']=='administrateur')) : ?>
+                                <div class="supp_btn">
+                                    <form class="btn" name="event_supp_form" id="event_supp_form" method="post" action="<?php print URL_PATH; ?>?page=evenements">
+                                        <input name="event<?php print $alertes[$i]->getId(); ?>" id="event<?php print $evenements[$i]->getId(); ?>" type="submit" value=""
+                                            onClick="confirm('Supprimer l\'événement?')" />
+                                    </form>
+                                </div>
+                            <?php endif; ?>
+                        </li>
+                    <?php }
+                } ?>
+            </ul>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6" id="alerte">
+        <div class="panel panel-danger">
+            <div class="panel-heading">
+                <h1 class="panel-title">Alertes</h1>
+            </div>
+            <ul class="list-group">
+                <?php if(isset($alertes)){
+                    for($i=0; $i<count($alertes); $i++){ ?>
+                        <li class="list-group-item">
+                            <div class="contenu">
+                                <span class="badge">Alerte</span>
+                                <?php print utf8_encode($alertes[$i]->getDateDebut()); ?> - 
+                                <strong><?php print utf8_encode($alertes[$i]->getTitre()); ?></strong> - 
+                                <?php print utf8_encode ($alertes[$i]->getDescription()); ?>
+                            </div>
+                            <?php if(isset($_SESSION['user_right']) && !empty($_SESSION['user_right']) && 
+                                    ($_SESSION['user_right']=='moderateur' || $_SESSION['user_right']=='administrateur')) : ?>
+                                <div class="supp_btn">
+                                    <form class="btn" name="alert_supp_form" id="alert_supp_form" method="post" action="<?php print URL_PATH; ?>?page=evenements">
+                                        <input name="alerte<?php print $alertes[$i]->getId(); ?>" id="alerte<?php print $alertes[$i]->getId(); ?>" type="submit" value="" 
+                                            onClick="confirm('Supprimer l\'alerte?')"/>
+                                    </form>
+                                </div>
+                            <?php endif; ?>
+                        </li>
+                    <?php } 
+                } ?>
+            </ul>
+        </div>
+    </div>
 </section>

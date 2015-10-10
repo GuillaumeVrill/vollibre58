f62bfb9a6b80dee34a90e5f0b3d6d3fb5b6ead22
Guillaume Vrilliaux <vrilliaux.guillaume@hotmail.com>
2015-10-10 15:05:11 +0200

    suppression des commentaires inutiles

--- controleurs/BlogControleur.php
+++ controleurs/BlogControleur.php
@@ -1,5 +1,3 @@
 <?php
 $page['vue'] = 'vue/corps_blog.php';
 $page['css'] = '<link rel="stylesheet" type="text/css" href="static/css/css_blog.css" />';
-
-// Actions et evenements:
--- controleurs/MentionsControleur.php
+++ controleurs/MentionsControleur.php
@@ -1,5 +1,3 @@
 <?php
 $page['vue'] = 'vue/corps_mentions.php';
 $page['css'] = '<link rel="stylesheet" type="text/css" href="static/css/css_mentions.css" />';
-
-// Actions et evenements:

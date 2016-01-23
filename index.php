7974bcc6ae27129cda10a3ef5aa77aa4bbd6d4df
Guillaume Vrilliaux <vrilliaux.guillaume@hotmail.com>
2016-01-23 14:34:32 +0100

    Masquage des erreurs et warnings

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
     session_start();
+    
+    // Désactiver le rapport d'erreurs
+    error_reporting(0);
+
+    // Rapporte les erreurs d'exécution de script
+    //error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+    // Rapporter les E_NOTICE peut vous aider à améliorer vos scripts
+    // (variables non initialisées, variables mal orthographiées..)
+    //error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    
     /*********** Front-controleur ****************************************
     *	Le front controleur est le point d'entrée du site, il appel 
     *	les autres controleurs et appel les fichiers de configuration

b2e015e26a0499927f6e86663bede3ff6d74a93f
Guillaume Vrilliaux <vrilliaux.guillaume@hotmail.com>
2016-02-08 10:38:54 +0100

    Ajout de l'espace reservé à la balise pioupiou (à l'heure actuelle, une balise de test)

--- vue/corps_accueil.php
+++ vue/corps_accueil.php
@@ -36,6 +36,41 @@
     </div>
 </section>
 <div class="row">
+    <div id="pioupiou" class="col-xs-12">
+        <h3>Donn&eacute;es m&eacute;t&eacute;os:</h3>
+        <?php
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "http://api.pioupiou.fr/v1/live/110");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $json_a = curl_exec($ch);
+            curl_close($ch);
+            $datas = json_decode($json_a, true);
+        ?>
+        <div class="pioupiou-datas">
+            <div class="pioupiou-left">
+                <h4>Informations g&eacute;n&eacute;rales:</h4>
+                <strong>Balise: </strong><?php print($datas['data']['meta']['name']); ?><br />
+                <strong>Lattitude: </strong><?php print($datas['data']['location']['latitude']); ?><br />
+                <strong>Longitude: </strong><?php print($datas['data']['location']['longitude']); ?><br />
+                <strong>Date: </strong><?php print($datas['data']['location']['date']); ?><br />
+            </div>
+            <div class="pioupiou-right">
+                <h4>Mesures:</h4>
+                <strong>Date: </strong><?php print($datas['data']['measurements']['date']); ?><br />
+                <strong>Pression: </strong><?php print($datas['data']['measurements']['pressure']); ?><br />
+                <strong>Vitesse Vent Moyenne: </strong><?php print($datas['data']['measurements']['wind_speed_avg']); ?><br />
+                <strong>Vitesse vent Max: </strong><?php print($datas['data']['measurements']['wind_speed_max']); ?><br />
+                <strong>Vitesse Vent Min: </strong><?php print($datas['data']['measurements']['wind_speed_min']); ?><br />
+            </div>
+            <div class="infos-legales">
+                Pour plus d'informations sur l'acquisition des donn&eacute;es des balises Pioupiou, se reporter &agrave; l'article correspondant dans les 
+                <a href="<?php print URL_PATH ?>?page=mentions" title="Mentions legales" target="_blank">Mentions l&eacute;gales</a>. <br />
+                Lien direct vers les donn&eacute;es brutes de la balise: <a href="http://api.pioupiou.fr/v1/live/110" target="_blank">ici</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
     <div id="video" class="col-xs-12 col-sm-8">
         <h3>Pr&eacute;sentation du club:</h3>
         <iframe src="<?php if(isset($video[0]) && notEmpty($video[0]->getChemin())){ print $video[0]->getChemin(); } ?>" frameborder="0" allowfullscreen></iframe>
